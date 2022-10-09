16ce9d7fa7c005329833252dc970bb9f28ae8491
Umar Asif <umarasif737@gmail.com>
2022-10-09 23:26:23 +0500

    Add project files.

---
+++ ConsoleApp2/Employee.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCal
+{
+    public abstract class Employee
+    {
+        double baseSal = 1500;
+        double fuelAllow; 
+        double medAllow;
+        public double salary;
+
+        public double BaseSalary
+        {
+            get
+            {
+                return baseSal;
+            }
+        }
+
+        public double FuelAllowance
+        {
+            set
+            {
+                fuelAllow = value;
+            }
+            get
+            { 
+                return fuelAllow; 
+            }
+        }
+
+        public double MedAllowance
+        {
+            set
+            {
+                medAllow = value;
+            }
+            get
+            {
+                return medAllow;
+            }
+        }
+
+        public abstract double CalSal();
+
+    }
+
+    public class Engineer : Employee
+    {
+        double annualSal = 7000;
+
+        public override double CalSal()
+        {
+            FuelAllowance = 0;
+            MedAllowance = 0;
+            salary = (annualSal / 12) + BaseSalary + FuelAllowance + MedAllowance;
+
+            return salary;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        double tax = 0.17;
+        public override double CalSal()
+        {
+            FuelAllowance = 250;
+            MedAllowance = 1000;
+            salary = BaseSalary + FuelAllowance + MedAllowance;
+            salary = salary - salary * tax;
+            return salary;
+        }
+
+    }
+    public class HR : Employee
+    {
+        double interAllowance;
+        public override double CalSal()
+        {
+            FuelAllowance = 150;
+            MedAllowance = 800;
+
+            interAllowance = (MedAllowance + FuelAllowance) * 0.10;
+            salary = BaseSalary + FuelAllowance + MedAllowance + interAllowance;
+
+            return salary;
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using SalaryCal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static Employee Prof(string prof)
+        {
+            if (prof == "Manager")
+            {
+                return new Manager();
+            }
+            else if (prof == "Engineer")
+            {
+                return new Engineer();
+            }
+            else
+            {
+                return new HR();
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's you proffesion: \n Engineer \n Manager \n HR");
+            string prof = Console.ReadLine();
+
+            Employee employee = Prof(prof);
+
+            Console.WriteLine("\nThe monthly salary of a " + prof + " is $" + employee.CalSal());
+
+            Console.ReadLine();
+        }
+    }
+}
